f8a4e120dd23bfd57961ecb25b2becd77ca141ea
Jakob Sahlström <jakob.sahlstrom@gmail.com>
2016-08-22 20:55:50 +0200

    Some clean up

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -7,24 +7,23 @@ $(document).ready(function(){
  		$.ajax({
  			url: '/animal_list_data'
  		}).done(function (data) {
- 			console.log(data);
  			data.animals.forEach(function (item) {
  				$('#gallery').append(
 					'<div class="col s12 m4">' +
-		  				'<div class="card">' +
-		    				'<div class="card-image">' +
-		      					'<img class="materialboxed" src="' + item.src + '">' +
-		      					'<span class="card-title">' + item.title + '</span>' +
-		    				'</div>' +
-		    				'<div class="card-content">' +
-		      					'<p>' + item.desc + '</p>' +
-		    				'</div>' +
-		  				'</div>' +
+						'<div class="card">' +
+							'<div class="card-image">' +
+								'<img class="materialboxed" src="' + item.src + '">' +
+								'<span class="card-title">' + item.title + '</span>' +
+							'</div>' +
+							'<div class="card-content">' +
+								'<p>' + item.desc + '</p>' +
+							'</div>' +
+						'</div>' +
 					'</div>'
- 					);
- 			});
-		    $('.materialboxed').materialbox();
- 		});
- 	});
+				);
+			});
+			$('.materialboxed').materialbox();
+		});
+	});
 });
 
